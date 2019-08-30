d16e60bec1728f394c6f71818819606d9bc073b2
Michael Copeland <cookiefries12@gmail.com>
2019-08-29 20:10:01 -0500

    Add some context to all three pages

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(() => {
+
+  $("#home").on('click', () => {
+    window.location.href = "./index.html";
+  })
+
+  $("#about").on('click', () => {
+    window.location.href = "./about.html";
+  })
+
+  $("#menu").on('click', () => {
+    window.location.href = "./menu.html";
+  })
+
+})
