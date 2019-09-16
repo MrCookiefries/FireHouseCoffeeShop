0538edf26c32885ba862d0ca9a0eebbcec474bce
Michael Copeland <cookiefries12@gmail.com>
2019-09-15 19:19:53 -0500

    Add quotes panel and fix mobile footer

--- script.js
+++ script.js
@@ -35,3 +35,52 @@ $(document).ready(() => {
     window.open("tel:+15732613707", "_blank");
   })
 })
+// Review Quotes
+let slideIndex = 0;
+
+function changeSlides(num) {
+  let quotes = {
+    amaya: {
+      words: "The support within the YEP community is endless, and has broadened my gratitude for humanity as a whole. Seeing the constant dedication to do the right thing for others can make a huge difference in the way a person perceives life. Helping others is a passion of mine, and YEP has made it easy for me.",
+      name: "Amaya Eades",
+      index: 0,
+      slide: 1
+    },
+    jameson: {
+      words: "Joining YEP was a stressful event for me, but it was one of the most important decisions of my life. I was not sure what to expect from the coffee shop, but what I got was a whole new family and even a new home. The experience of joining YEP has changed me as a person and for that I am grateful. I wouldn’t trade the crew I’ve become a part of for anything. The coffee shop benefited me so greatly, I continued volunteering there even after I graduated from highschool. The coffee shop creates an important experience for anyone who enters its doors. The people I’ve encountered working at the coffee shop have become some of the most important people in my life.",
+      name: "Jameson Vannatta",
+      index: 1,
+      slide: 2
+    },
+    amber: {
+      words: "To serve others in a supportive environment and make a positive impact on the community is something I’ll never forget. YEP was a place of personal growth, friendship, leadership, and a great learning experience.",
+      name: "Amber Copeland",
+      index: 2,
+      slide: 3
+    }
+  }
+  slideIndex += num;
+  if (slideIndex > 2) {
+    slideIndex = 0;
+  }
+  switch (slideIndex) {
+    case 0:
+      $('#quote').html(quotes.amaya.words);
+      $('#quote-author').html(quotes.amaya.name);
+      $('#slide-number').html(quotes.amaya.slide);
+      break;
+    case 1:
+      $('#quote').html(quotes.jameson.words);
+      $('#quote-author').html(quotes.jameson.name);
+      $('#slide-number').html(quotes.jameson.slide);
+      break;
+    case 2:
+      $('#quote').html(quotes.amber.words);
+      $('#quote-author').html(quotes.amber.name);
+      $('#slide-number').html(quotes.amber.slide);
+      break;
+    default:
+      console.log("Error with quotes panel.");
+      break;
+  }
+}
